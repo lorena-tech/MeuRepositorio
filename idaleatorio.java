9185f5c0f61cba7c0a1bc41a9efac858a29b8807
Lorena dos Santos <118130696+lorena-tech@users.noreply.github.com>
2023-08-09 09:10:16 -0300

    Update idaleatorio.java

--- idaleatorio.java
+++ idaleatorio.java
@@ -6,6 +6,11 @@ class CalcularHoras{
       //cria um objeto que sera utilizado para criar o id de consulta
       Random consulta = new Random();
       
-      
+     //garante que todos os numeros de consulta serao positivos
+      for (int i = 0; i < 1; i++) {
+            int protocolo = consulta.nextInt(1000)*1234;
+            
+            System.out.println("Por favor, anote o seu protocolo "+protocolo);
+      }
     }
 }
